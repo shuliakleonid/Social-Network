c6544697400d1a25b3e82d1ac80974e79dd7d66b
shuliak leonid <zeleny777@gmail.com>
2021-09-08 08:28:56 +0300

    add profile info  #97

--- src/pages/Music/Music.tsx
+++ src/pages/Music/Music.tsx
@@ -1,10 +1,6 @@
-import React from 'react';
+import React from "react";
 
-const  Music=()=> {
-  return (
-      <div>
-        Music
-      </div>
-  )
-}
+const Music = () => {
+  return <div>Music</div>;
+};
 export default Music;
